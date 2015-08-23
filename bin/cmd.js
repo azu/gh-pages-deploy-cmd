65134225343b6c6ed10776eb3f13544836fcadc1
azu <azuciao@gmail.com>
2015-08-23 16:22:24 +0900

    Update 2015-08-23T07:22:22.941Z

--- bin/cmd.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-var deploy = require("../lib/gh-pages-deploy");
-var globs = process.argv[2];
-deploy({
-    globs: globs
-}).catch(function (erorr) {
-    console.error(error, erorr.stack);
-    process.exit(1);
-});
