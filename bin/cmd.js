3b91737785578779ca9a697b23b8cacf2a8d3077
azu <azuciao@gmail.com>
2015-08-23 20:43:29 +0900

    fix(git): use direct git command

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var deploy = require("../lib/gh-pages-deploy");
 var globs = process.argv[2];
-deploy(globs).on("error", function (error) {
+deploy(globs).catch(function (error) {
     console.error(error, error.stack);
     process.exit(1);
 });
